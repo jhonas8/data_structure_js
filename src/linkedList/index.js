677abddf0cb76a4a99cdea0012b2acb6385350c4
jhonas8 <love.android62@gmail.com>
2023-04-08 11:39:38 -0300

    Adiciona metodos simples relacionados ao indice

--- src/linkedList/index.js
+++ src/linkedList/index.js
@@ -1,7 +1,5 @@
 import { ListNode } from "./listNode";
 
-// test signature
-
 export class LinkedList {
   count;
   head;
@@ -11,12 +9,53 @@ export class LinkedList {
     this.head = undefined;
   }
 
-  static equals(elementA, elementB, customFunction) {
+  setEqual(newEquals) {
+    this.equals = newEquals;
+  }
+
+  equals(elementA, elementB, customFunction) {
     if (!elementA || !elementB) return;
 
     if (!customFunction) return elementA === elementB;
 
     customFunction(elementA, elementB);
+  }
+
+  indexOf(el) {
+    if (this.equals(this.head, el)) return 0;
+    let currentElement = this.head.next;
+    let index = 1;
+
+    while (!this.equals(currentElement, el)) {
+      if (current == undefined) return -1;
+      index++;
+      currentElement = current.next;
+    }
+
+    return index;
+  }
+
+  getElementAt(index) {
+    if (index === 0) return this.head;
+
+    if (index > this.size()) return undefined;
+
+    let current = this.head.next;
+    for (let i = 1; i < index; i++) current = current.next;
+
+    return current;
+  }
+
+  insert(el, index) {
+    const node = new Node(el);
+
+    const currentElementBeforeTheIndex = this.getElementAt(index - 1);
+    const currentElementAtTheIndex = currentElementBeforeTheIndex.next;
+
+    currentElementBeforeTheIndex.next = node;
+    node.next = currentElementAtTheIndex;
+
+    return node;
   }
 
   isEmpty() {
